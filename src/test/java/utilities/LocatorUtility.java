835a15e19dcff8c27a802dead6b1b730e1506890
Anusuya Samidurai <161169113+AnusuyaSamidurai@users.noreply.github.com>
2024-04-03 23:23:30 -0400

    Update LocatorUtility.java

--- src/test/java/utilities/LocatorUtility.java
+++ src/test/java/utilities/LocatorUtility.java
@@ -7,7 +7,6 @@ public  class LocatorUtility {
 	
 	public static void conceptsClick(String concept, WebDriver driver) {
 		By conceptLocator = By.xpath("//a[contains(text(),'"+concept+"')]");
-		//wait.until(ExpectedConditions.elementToBeClickable(treeConcepts)).click();
 		driver.findElement(conceptLocator).click();
 	}
 
